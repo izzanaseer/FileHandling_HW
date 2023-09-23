65aae07955db171592e17c1e3c158ddd2dcb383e
izzanaseer <izzanaseer3@gmail.com>
2023-09-23 22:42:10 +0500

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filePath = "applicant.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length >= 2) {
+                    String name = parts[0];
+                    System.out.println("Applicant: " + name);
+
+                    for (int i = 1; i < parts.length; i++) {
+                        String[] skillData = parts[i].split(":");
+                        if (skillData.length == 2) {
+                            String skill = skillData[0];
+                            int experience = Integer.parseInt(skillData[1]);
+                            System.out.println("  Skill: " + skill + ", Experience: " + experience + " years");
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
